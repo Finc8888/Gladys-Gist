63fa52a1f336571c1a4d1b53752fe46dbde70882
friedfox <finc8888@gmail.com>
2026-04-12 22:31:55 +0300

    Version 4: Customizing HTTP headers: Customizing headers

--- main.go
+++ main.go
@@ -25,10 +25,10 @@ func gistView(w http.ResponseWriter, r *http.Request) {
 // Add a gistCreate handler function.
 func gistCreate(w http.ResponseWriter, r *http.Request) {
     if r.Method != "POST" {
-        // If it's not, use the w.WriteHeader() method to send a 405 status
-        // code and the w.Write() method to write a "Method Not Allowed"
-        // response body. We then return from the function so that the
-        // subsequent code is not executed.
+        // Use the Header().Set() method to add an 'Allow':POST' header to the
+        // response header map. The first parameter is the header name, and
+        // the second parameter is the header value.
+        w.Header().Set("Allow", "POST")
         w.WriteHeader(405)
         w.Write([]byte("Method Not Allowed"))
         return
