81f27a523bbce7766427bb85a5426583e5b41e59
Finc8888 <finc8888@gmail.com>
2026-06-12 23:48:06 +0300

    sync

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,17 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Create a file server which serves files ot of the "./ui/static" directory.
+	// Note that the path given to the http.Dir function is relative to the project direcotory root
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Register the other application routes as normal
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/gist/view", gistView)
 	mux.HandleFunc("/gist/create", gistCreate)
