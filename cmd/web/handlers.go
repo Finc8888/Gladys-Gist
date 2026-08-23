e586ac81f2d7d946dbc88ca9d00e99ccf9bd9adc
Finc8888 <finc8888@gmail.com>
2026-08-23 16:03:36 +0300

    fix(templates): Fix and refactor templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -18,9 +18,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// to note that the file containing our base template must be the *first*
 	// file in the slice.
 	files := []string{
-	    "./ui/html/base.tmpl",
-	    "./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/home.tmpl",
+	    "./ui/html/base.tmpl.html",
+	    "./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
 	}
 
 	// Use the template.ParseFiles() function to read the files and store the
